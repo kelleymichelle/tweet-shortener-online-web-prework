f2b6e6747ad257dbc08b55fc938cc3eb31bf406d
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-23 05:49:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
- word_substituter(tweet)
+  word_substituter(tweet)
   if tweet.length > 140
     tweet = tweet[1..137] + "..."
       puts tweet
